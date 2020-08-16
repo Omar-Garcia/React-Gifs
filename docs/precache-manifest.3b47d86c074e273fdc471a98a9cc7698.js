1a766fe99d719bf4551da604a54589cf8312dc50
Omar Garcia <omar.garcia@sigob.com.mx>
2020-08-16 14:56:47 -0700

    Se agrego docs

---
+++ docs/precache-manifest.3b47d86c074e273fdc471a98a9cc7698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bd1297dfa0725273138242c9e6f025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1bceb5bd8fe4b890a56",
+    "url": "/static/css/main.be341641.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bceb5bd8fe4b890a56",
+    "url": "/static/js/main.e95dd43d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
